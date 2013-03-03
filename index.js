7f31307841dbf908b3b1461d0bf661a9d6ee846c
andrej ocenas <andrej.ocenas.mail@gmail.com>
2013-03-03 12:10:47 +0100

    rules moved to separate file, added xmlparts for snippet insertion in rules

--- index.js
+++ index.js
@@ -1,69 +1,35 @@
 var fs = require('fs');
 var util = require('util');
 var et = require('elementtree');
+var rules = require('./rules').rules;
 
 
-exports = module.exports = {};
+var changeCounter = Object.create(null);
 
-var esc = function (str) {
-  return str
-    .replace(/\//g, '\\/')
-    .replace(/\"/g, '\\\"')
-    .replace(/\'/g, '\\\'');
-};
-
-var rules = [
-  {
-    name: 'debug',
-    active: true,
-    node: {
-      name: 'stringProp',
-      attr: {
-        name: 'HTTPSampler.path'
-      }
-    },
-    xpath: 'stringProp[@name="HTTPSampler.path"]',
-    fn: function (el) { 
-      console.log(util.inspect(el)); 
-    }
-  },
-  {
-    name: 'paydom_mid_replace',
-    active: true,
-    node: {
-      name: 'stringProp',
-      attr: { 
-        name: 'HTTPSampler.path'
-      }
-    },
-    xpath: 'stringProp[@name="HTTPSampler.path"]',
-    fn: function (el) {
-      var re = /\/mid\/\d+/i; // example: '...../mid/12'
-      el.text = el.text.replace(re, '/mid/${PAYDOM_MID}');
-    }
-  }
-];
-
-
-exports.esc = esc;
-exports.rules = rules;
-
-var fileName = './test.xml';
-//var fileName = './TEST_EXECUTOR_R4.jmx';
+//var fileName = 'test.xml';
+//var fileName = 'TEST_EXECUTOR_R4.jmx';
+var fileName = 'assertion_test.xml';
 
 data = fs.readFileSync(fileName).toString();
 etree = et.parse(data);
 
 rules.forEach(function (rule) {
   if (rule.active) {
-    etree.findall(rule.xpath).forEach(function (el) {
+    var elements = etree.findall(rule.xpath);
+    //console.log(elements);
+    elements.forEach(function (el) {
+      var original = el.text;
       rule.fn(el);
+      if (original !== el.text) {
+        changeCounter[rule.name] = changeCounter[rule.name] || 0;
+        changeCounter[rule.name]++;
+      }
     });
   }
 });
 
 
-
-console.log(etree.write());
-fs.writeFileSync(fileName + '.new', etree.write({indent: 2}));
+console.log(util.inspect(changeCounter));
+//console.log(etree.write());
+fs.writeFileSync('new_' + fileName, etree.write({indent: 2}));
 
