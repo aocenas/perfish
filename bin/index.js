1d831c5e87c026b041855e2e8e0c823c21ea9b66
andrej ocenas <andrej.ocenas.mail@gmail.com>
2013-03-03 16:50:12 +0100

    options description

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
+
 var argv = require('optimist')
   .usage('Usage: $0 [-c <controller name>] <filename>')
-  .describe('c', 'optional; name of the controler, \
-            where the changes will be made')
-  .describe('<filename>', 'required; name of the xml file')
+  .demand(1)
+  //TODO find out how to better format these descriptions
+  .describe('c', 'optional; not implemented yet; name of the controller,\n' +
+            '\t\twhere the changes will be made. this will limit\n' +
+            '\t\tthe scope on which the rules are applied.\n')
+  .describe('<filename>', 'required; name of the xml file.')
   .argv;
 var perfish = require('../index.js');
 
-if (argv._.length === 0) { 
-  console.log('<filename> is required'); 
-  return 1;
-}
-
 perfish.go(argv.c, argv._[0]);
-
 
