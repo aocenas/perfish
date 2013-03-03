f479dda71f7b7e5e02a6b6a0f988e10a8c0dd48e
andrej ocenas <andrej.ocenas.mail@gmail.com>
2013-03-03 13:13:10 +0100

    main func exported

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 var fs = require('fs');
 var util = require('util');
 var et = require('elementtree');
-var rules = require('./rules').rules;
+var path = require('path');
+
+exports = module.exports = {};
+
+var go = function (controler, fileName) {
+  var rules = require('./lib/rules.js').rules;
+
+  var changeCounter = Object.create(null);
+
+  //var fileName = 'test.xml';
+  //var fileName = 'TEST_EXECUTOR_R4.jmx';
+  //var fileName = 'assertion_test.xml';
+
+  data = fs.readFileSync(fileName).toString();
+  etree = et.parse(data);
+
+  rules.forEach(function (rule) {
+    if (rule.active) {
+      var elements = etree.findall(rule.xpath);
+      //console.log(elements);
+      elements.forEach(function (el) {
+        var original = el.text;
+        rule.fn(el);
+        if (original !== el.text) {
+          changeCounter[rule.name] = changeCounter[rule.name] || 0;
+          changeCounter[rule.name]++;
+        }
+      });
+    }
+  });
 
 
-var changeCounter = Object.create(null);
+  console.log(util.inspect(changeCounter));
+  //console.log(etree.write());
+  fs.writeFileSync('new_' + path.basename(fileName), etree.write({indent: 2}));
 
-//var fileName = 'test.xml';
-//var fileName = 'TEST_EXECUTOR_R4.jmx';
-var fileName = 'assertion_test.xml';
+}
 
-data = fs.readFileSync(fileName).toString();
-etree = et.parse(data);
-
-rules.forEach(function (rule) {
-  if (rule.active) {
-    var elements = etree.findall(rule.xpath);
-    //console.log(elements);
-    elements.forEach(function (el) {
-      var original = el.text;
-      rule.fn(el);
-      if (original !== el.text) {
-        changeCounter[rule.name] = changeCounter[rule.name] || 0;
-        changeCounter[rule.name]++;
-      }
-    });
-  }
-});
-
-
-console.log(util.inspect(changeCounter));
-//console.log(etree.write());
-fs.writeFileSync('new_' + fileName, etree.write({indent: 2}));
-
+exports.go = go;
